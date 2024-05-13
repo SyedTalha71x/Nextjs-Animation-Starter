04025f26d50832cef82e6c535cb80dc86d718c4b
SyedTalha71x <syedtalha71x@gmail.com>
2024-05-13 12:55:32 +0500

    Manage Extension Pack for Particles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import Homepage from '../app/Homepage/page'
 import Animation from '../app/Particles/page'
 import Cursor from '../app/Customcursor/page'
 import Hero from '../app/Hero/page'
-import Slider from '../app/Slider/page'
 
 export default function Home() {
   return (
@@ -11,7 +10,6 @@ export default function Home() {
       <Cursor />
       <Animation />
       <Hero />
-      {/* <Slider /> */}
     </>
   );
 }
